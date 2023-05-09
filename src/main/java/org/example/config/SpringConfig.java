bffdebf491bd935cbd40e7058a41a4b04604ab6d
GorbunovIvan <gorbunovvivan@gmail.com>
2023-05-09 09:33:10 +0300

    init

---
+++ src/main/java/org/example/config/SpringConfig.java
@@ -0,0 +1,61 @@
+package org.example.config;
+
+import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("org.example")
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+
+    private final ApplicationContext context;
+
+    public SpringConfig(ApplicationContext context) {
+        this.context = context;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        var templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setApplicationContext(context);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        var templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        var viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        registry.viewResolver(viewResolver);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/spring-mvc-social-network");
+        dataSource.setUser("postgres");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+}
